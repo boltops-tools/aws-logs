c742142ed68b243a19b472a936892c25add3b32d
Tung Nguyen <tongueroo@gmail.com>
2019-11-27 19:56:44 +0000

    simple single events api call spec

--- lib/aws_logs/tail.rb
+++ lib/aws_logs/tail.rb
@@ -11,11 +11,11 @@ module AwsLogs
       set_trap
     end
 
+    @@end_loop_signal = false
     def set_trap
       Signal.trap("INT") {
         puts "\nCtrl-C detected. Exiting..."
-        sleep 0.1
-        exit
+        @@end_loop_signal = true
       }
     end
 
@@ -25,8 +25,29 @@ module AwsLogs
       @completed = nil
     end
 
+    # So the start and end time will limit results and make the API fast.
+    # 1. load all events from since time
+    # 2. after that load events from the last found event or time now now?
+    # 3. time now is even better
+    #
+    # However, events can still be duplicated as events can be written to the exact same timestamp.
+    # So also track the last_shown_event_id and prevent duplicate log lines from re-appearing.
+    #
+    def run
+      since, now = initial_since, current_now
+      puts "end_loop? #{end_loop?.inspect}"
+      while true && !end_loop?
+        refresh_events(since, now)
+        display
+        puts "=" * 30
+        since, now = now, current_now
+        loop_count!
+        sleep 5 unless ENV["AWS_LOGS_TEST"]
+      end
+    end
+
     def refresh_events(start_time, end_time)
-      puts "refresh_events called"
+      puts "refresh_events called".color(:green)
       @events = []
       next_token = :start
 
@@ -38,6 +59,10 @@ module AwsLogs
           end_time: end_time,
           # limit: 10,
         )
+
+        # puts "resp:"
+        # pp resp
+
         @events += resp.events
         next_token = resp.next_token
       end
@@ -54,26 +79,23 @@ module AwsLogs
       (Time.now.to_i) * 1000 # now in milliseconds
     end
 
-    # So the start and end time will limit results and make the API fast.
-    # 1. load all events from since time
-    # 2. after that load events from the last found event or time now now?
-    # 3. time now is even better
-    #
-    # However, events can still be duplicated as events can be written to the exact same timestamp.
-    # So also track the last_shown_event_id and prevent duplicate log lines from re-appearing.
-    #
-    def run
-      since, now = initial_since, current_now
-      while true && !$end_loop
-        refresh_events(since, now) # pass 1
-        display
-        puts "=" * 30
-        since, now = now, current_now
-        sleep 5
-      end
+    @@loop_count = 0
+    def end_loop?
+      return true if @@end_loop_signal
+      max_loop_count && @@loop_count >= max_loop_count
+    end
+
+    def loop_count!
+      @@loop_count += 1
+    end
+
+    # Useful for specs
+    def max_loop_count
+      nil
     end
 
     def display
+      puts "display:"
       new_events = @events
       shown_index = new_events.find_index { |e| e.event_id == @last_shown_event_id }
       if shown_index
@@ -82,9 +104,13 @@ module AwsLogs
 
       new_events.each do |e|
         time = Time.at(e.timestamp/1000).utc
-        puts "#{time.to_s.color(:green)} #{e.log_stream_name.color(:purple)} #{e.message}"
+        say "#{time.to_s.color(:green)} #{e.log_stream_name.color(:purple)} #{e.message}"
       end
       @last_shown_event_id = @events.last&.event_id
+    end
+
+    def say(text)
+      puts text
     end
   end
 end
--- spec/lib/tail_spec.rb
+++ spec/lib/tail_spec.rb
@@ -1,48 +1,47 @@
+module AwsLogs
+  class Tail
+    # Override to 1 as default. Override on a per spec level if needed.
+    def max_loop_count
+      1
+    end
+  end
+end
+
+Rainbow.enabled = false
+
 describe AwsLogs::Tail do
   let(:tail) do
     tail = AwsLogs::Tail.new
     allow(tail).to receive(:cloudwatchlogs).and_return(cloudwatchlogs)
     tail
   end
-
   let(:cloudwatchlogs) do
     logs = double(:logs).as_null_object
-    # allow(logs).to receive(:filter_log_events).and_return(
-    #   mock_response("spec/fixtures/typical/events-1.json", next_token: true),
-    #   mock_response("spec/fixtures/typical/events-2.json"),
-    #   # mock_response("spec/fixtures/typical/events-3.json"),
-    #   # mock_response("spec/fixtures/typical/events-4.json"),
-    # )
-
-    allow(logs).to receive(:filter_log_events) do |args|
-      puts "args #{args.inspect}"
-      if args[:start_time] == 1574888800000 && args[:end_time] == 1574888815000
-        puts "hi1"
-        mock_response("spec/fixtures/typical/events-2.json")
-      else
-        puts "hi2"
-        $end_loop = true
-        mock_response("spec/fixtures/typical/events-3.json")
-      end
-    end
-
-    # allow(obj).to receive(:message) do |arg1, arg2|
-    #   # set expectations about the args in this block
-    #   # and/or return  value
-    # end
+    allow(logs).to receive(:filter_log_events).and_return(*filter_log_events_response)
     logs
   end
 
-  describe "tail" do
-    it "tail" do
-      allow(tail).to receive(:initial_since).and_return(1574888800000)
-      allow(tail).to receive(:current_now).and_return(
-        1574888815000,
-        1574888825000,
-        1574888835000,
-        1574888845000,
-      )
-      tail.run
+  context "1 filter_log_events with single events api call" do
+    let(:filter_log_events_response) do
+      [
+        mock_response("spec/fixtures/typical/events-2.json"),
+      ]
+    end
+
+    describe "tail" do
+      it "run" do
+        # override to capture output
+        out = []
+        allow(tail).to receive(:say) { |text| out << text }
+
+        tail.run
+
+        text = out.join("\n") + "\n"
+        expect(text).to eq(<<~EOL)
+         2019-11-27 21:06:50 UTC stream-name message1
+         2019-11-27 21:07:00 UTC stream-name message2
+        EOL
+      end
     end
   end
 end
