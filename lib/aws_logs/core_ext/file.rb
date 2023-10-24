979a1e252352211c0d6b4bf4f7e10bb95ec42ecc
Tung Nguyen <tongueroo@gmail.com>
2023-10-24 23:51:11 +0000

    alias File.exists?

---
+++ lib/aws_logs/core_ext/file.rb
@@ -0,0 +1,11 @@
+class File
+  class << self
+    # Ruby 3.2 removed File.exists?
+    # aws_config/store.rb uses it
+    # https://github.com/a2ikm/aws_config/blob/ef9cdd0eda116577f7d358bc421afd8e2f1eb1d3/lib/aws_config/store.rb#L6
+    # Probably a bunch of other libraries still use File.exists? also
+    unless method_defined?(:exist?)
+      alias_method :exists?, :exist?
+    end
+  end
+end
